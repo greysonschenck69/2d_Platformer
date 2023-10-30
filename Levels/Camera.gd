d63c052f62e4fa92b249e9cd0982a8a151f57692
greysonschenck69 <greysonschenck@yahoo.com>
2023-10-30 12:21:51 -0400

    update

---
+++ Levels/Camera.gd
@@ -0,0 +1,20 @@
+extends Camera2D
+
+var player = null
+
+var death_zone = 1000
+
+func _ready():
+	death_zone = limit_bottom + 200
+	enabled = true
+
+
+func _physics_process(_delta):
+	player = get_node_or_null("/root/Game/Player_Container/Player")
+	if player != null:
+		zoom = Vector2(2.0,2.0)
+		global_position = player.global_position
+	else:
+		zoom = Vector2(1,1)
+		position = Vector2(0,0)
+		
